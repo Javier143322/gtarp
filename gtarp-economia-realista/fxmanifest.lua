181770bd16e446fe0c7626d367bd5dd7d0a54729
and_res_4 <huhdnuhdl@gmail.com>
2025-10-22 12:27:46 -0500

    Actualizar fxmanifest.lua

--- gtarp-economia-realista/fxmanifest.lua
+++ gtarp-economia-realista/fxmanifest.lua
@@ -1,1 +1,31 @@
+fx_version 'cerulean'
+game 'gta5'
 
+author 'TuNombre'
+description 'Sistema Completo de Economía Realista y Propiedades'
+version '2.0.0'
+
+server_scripts {
+    '@mysql-async/lib/MySQL.lua',
+    'config.lua',
+    'server/main.lua',
+    'server/economia.lua',
+    'server/propiedades.lua',
+    'server/servicios.lua',
+    'server/transacciones.lua'
+}
+
+client_scripts {
+    'config.lua',
+    'client/menus.lua',
+    'client/marcadores.lua'
+}
+
+shared_scripts {
+    'shared/funciones.lua'
+}
+
+dependencies {
+    'es_extended',
+    'mysql-async'
+}
